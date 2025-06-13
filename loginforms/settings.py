672923af4a7be220503b3f4f2073244ae4dc61dd
RECEPTION GTTH <RECEPTION@ghurkitrust.org.pk>
2025-06-13 17:05:25 +0500

    modfify files

--- loginforms/settings.py
+++ loginforms/settings.py
@@ -10,6 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
+
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
